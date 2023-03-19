904aaab850e4b7a580af7b86ca48b7e0b38884a1
Samantha Genevay <samanthagenevay@gmail.com>
2023-03-19 11:32:53 -0700

    Completed Lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,22 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        print(first_name, last_name)
     
-    def learn(self):
-        pass
+    def learn(self, more_knowledge):
+        for info in more_knowledge:
+            self.knowledge.append(more_knowledge)
+        print(self.knowledge)
+
+
+sam = Student("Sam", "G")
+sam.learn("SO much knowledge")
+
+
+
+
+        
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,38 @@ from user import User
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+
+
     def teach(self):
-        pass
+        random_int = random.randint(0,7)
+        self.knowledge = self.knowledge[random_int]
+        print(self.knowledge)
+
+
+    def print_teacher_details(self):
+        print(f'''
+            first_name: {self.first_name}
+            last_name: {self.last_name}
+            knowledge: {self.knowledge}
+        ''')
+
+my_teacher = Teacher("John", "Smith", knowledge)
+my_teacher.teach()
+my_teacher.print_teacher_details()
